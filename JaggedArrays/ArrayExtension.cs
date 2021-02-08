85d926f92e3c4059c45ca67270a9192eac64fdb1
Farhad <epam@farhad.su>
2021-02-08 10:30:03 +0300

    All done!

--- JaggedArrays/ArrayExtension.cs
+++ JaggedArrays/ArrayExtension.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 #pragma warning disable S2368
 
@@ -13,7 +13,39 @@ namespace JaggedArrays
         /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
         public static void OrderByAscendingBySum(this int[][] source)
         {
-            throw new NotImplementedException();
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            for (int i = source.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (GetArraySum(source[j]) > GetArraySum(source[j + 1]))
+                    {
+                        var temp = source[j];
+                        source[j] = source[j + 1];
+                        source[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public static int GetArraySum(int[] array)
+        {
+            if (array is null)
+            {
+                return -1;
+            }
+
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += array[i];
+            }
+
+            return result;
         }
         
         /// <summary>
@@ -23,7 +55,23 @@ namespace JaggedArrays
         /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
         public static void OrderByDescendingBySum(this int[][] source)
         {
-            throw new NotImplementedException();
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            for (int i = source.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (GetArraySum(source[j]) < GetArraySum(source[j + 1]))
+                    {
+                        var temp = source[j];
+                        source[j] = source[j + 1];
+                        source[j + 1] = temp;
+                    }
+                }
+            }
         }
         
         /// <summary>
@@ -33,7 +81,42 @@ namespace JaggedArrays
         /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
         public static void OrderByAscendingByMax(this int[][] source)
         {
-            throw new NotImplementedException();
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            for (int i = source.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (GetMaxValue(source[j]) > GetMaxValue(source[j + 1]))
+                    {
+                        var temp = source[j];
+                        source[j] = source[j + 1];
+                        source[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public static int GetMaxValue(int[] array)
+        {
+            if (array is null)
+            {
+                return -1;
+            }
+
+            int result = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (result < array[i])
+                {
+                    result = array[i];
+                }
+            }
+
+            return result;
         }
         
         /// <summary>
@@ -43,7 +126,23 @@ namespace JaggedArrays
         /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
         public static void OrderByDescendingByMax(this int[][] source)
         {
-            throw new NotImplementedException();
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            for (int i = source.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (GetMaxValue(source[j]) < GetMaxValue(source[j + 1]))
+                    {
+                        var temp = source[j];
+                        source[j] = source[j + 1];
+                        source[j + 1] = temp;
+                    }
+                }
+            }
         }
     }
 }
